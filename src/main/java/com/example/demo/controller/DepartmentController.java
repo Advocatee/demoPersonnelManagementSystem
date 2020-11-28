ea74977e9f588d6394f2b673517bf69a911325e1
Roman <All.Under.Control@mail.ru>
2020-11-28 20:38:33 +0300

    add GetById

--- src/main/java/com/example/demo/controller/DepartmentController.java
+++ src/main/java/com/example/demo/controller/DepartmentController.java
@@ -8,6 +8,7 @@ import com.example.demo.service.Impl.DepartmentServiceImpl;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class DepartmentController {
@@ -28,10 +29,11 @@ public class DepartmentController {
         return departmentDtoList;
     }
 
-//    @GetMapping(value = "/department/{id}")
-//    public DepartmentDto getOneDepartment(@PathVariable UUID id) {
-//        return departmentMapper.getDepartment(id);
-//    }
+    @GetMapping(value = "/department/{id}")
+    public DepartmentDto getOneDepartment(@PathVariable UUID id) {
+        Department departmentById = departmentService.getDepartmentById(id);
+        return mapper.toDepartmentDto(departmentById);
+    }
 
     @PostMapping("/createDepartment")
     public DepartmentDto createDepartment(@RequestBody CreateDepartmentRequest createDepartmentRequest) {
